64cfa8257309df6cdf15b0a5fcfa0d55acff677b
Kendra Kyndberg <kendrak922@gmail.com>
2018-03-17 18:43:13 -0500

    added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,21 +8,87 @@ const questions = {
         correct: "The Eyes of God",
     }],
  questionTwo: [{
+    //  what does miss Havisham wear in Great Expectations?
     choiceA:"her old school uniform",
     choiceB:"potato sack",
     choiceC:"sailor outfit",
     correct: "a wedding dress",
 }],
 questionThree: [{
+    // Who wrote all quiet on the Western Front?
     choiceA:"Norman Mailor",
     choiceB:"Erich Von Schlep",
     choiceC:"Ernest Hemmingway",
     correct: "Erich Maria Remarque",
-}]
+}],
+questionFour: [{
+    // Who does Romeo love in the first scene?
+    choiceA:"Juliet",
+    choiceB:"Lady Montague",
+    choiceC:"Lady Capulet",
+    correct:"Rosaline",
+}],
+questionFive: [{
+    // Which of the following items does Scout NOT find in a knothole of a tree?
+    choiceA:"gum",
+    choiceB:"a spelling medal",
+    choiceC:"a ball of twine",
+    correct:"a small toy car",
+}],
+questionFive: [{
+    // Which of the following items does Scout NOT find in a knothole of a tree?
+    choiceA:"gum",
+    choiceB:"a spelling medal",
+    choiceC:"a ball of twine",
+    correct:"a small toy car",
+}],
+questionSix: [{
+    //What excites Lennie most about his dream life with George??
+    choiceA:"He wants to get back to his roots",
+    choiceB:"George promises to introduce him to some hot chicks",
+    choiceC:"He will be rich from panninf for gold",
+    correct:"He will get to tend rabbits",
+}],
+questionSeven: [{
+    //What is a "big cat" in the novel "Grapes of Wrath"??
+    choiceA:"A terrible dust storm",
+    choiceB:"A migrants' nickname for a policeman",
+    choiceC:"the head farmer",
+    correct:"A machine used by the banks to evict farmers",
+}],
+questionEight: [{
+    //What earns Fahrenheit 451 its title??
+    choiceA:"The main character's nickname",
+    choiceB:"the oversight agency of the government",
+    choiceC:"Taken from the name of the fire department",
+    correct:"It's the temperature at which books burn",
+}],
+questionNine: [{
+    //What type of animal is Boxer in Animal Farm??
+    choiceA:"Sheep",
+    choiceB:"Dog",
+    choiceC:"Pig",
+    correct:"Horse",
+}],
+questionTen: [{
+    //	Complete the quotation: “It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a ___.”??
+    choiceA:"Title",
+    choiceB:"Larger fortune",
+    choiceC:"House",
+    correct:"Wife",
+}],
 };
 
-// questions.questionOne[0].choiceA
+
+for( i = 0, i >= questions.length, i++){
+let a = 
+
+
+
+}
+
+
 $(".form-check-label").append(questions.questionOne[0].choiceA)
-$(".form-check-label").append(questions.questionOne[0].choiceB)
-$(".form-check-label").append(questions.questionOne[0].choiceC)
-$(".form-check-label").append(questions.questionOne[0].correct)
+$(".form-check-label2").append(questions.questionOne[0].choiceB)
+$(".form-check-label3").append(questions.questionOne[0].choiceC)
+$(".form-check-label4").append(questions.questionOne[0].correct)
