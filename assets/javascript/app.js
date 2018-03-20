8177e5dca0129ad875645b1254eb391be74ca3a6
Kendra Kyndberg <kendrak922@gmail.com>
2018-03-20 15:03:58 -0500

    appended questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,124 +1,177 @@
 'use strict';
 $(document).ready(function(){
-const questions = {
-questionOne : {
-        question : "What do the “eyes of Dr. T.J. Eckleburg “represent in The Great Gatsby?",
-        choiceA : "The Male Gaze",
-        choiceB : "The Green Light",
-        choiceC : "Gatsby's subconscious",
-        correct : "The Eyes of God",
-    },
-questionTwo : {
-    question : "what does miss Havisham wear in Great Expectations?",
-    choiceA : "her old school uniform",
-    choiceB : "potato sack",
-    choiceC : "sailor outfit",
-    correct : "a wedding dress",
- },
-questionThree : {
-    question : "Who wrote all quiet on the Western Front?",
-    choiceA : "Norman Mailor",
-    choiceB : "Erich Von Schlep",
-    "choiceC" : "Ernest Hemmingway",
-    "correct" : "Erich Maria Remarque",
-},
-questionFour : {
-    question : "Who does Romeo love in the first scene?",
-    choiceA : "Juliet",
-    choiceB : "Lady Montague",
-    choiceC : "Lady Capulet",
-    correct : "Rosaline",
-},
-questionFive : {
-    question : "Which of the following items does Scout NOT find in a knothole of a tree?",
-    choiceA : "gum",
-    choiceB : "a spelling medal",
-    choiceC : "a ball of twine",
-    correct : "a small toy car",
-},
-questionSix : {
-    question : "What excites Lennie most about his dream life with George?",
-    choiceA : "He wants to get back to his roots",
-    choiceB : "George promises to introduce him to some hot chicks",
-    choiceC : "He will be rich from panninf for gold",
-    correct : "He will get to tend rabbits",
-},
-questionSeven : {
-    question : "What is a “big cat“ in the novel Grapes of Wrath",
-    choiceA : "A terrible dust storm",
-    choiceB : "A migrants' nickname for a policeman",
-    choiceC : "the head farmer",
-    correct : "A machine used by the banks to evict farmers",
-},
-questionEight : {
-   question : "What earns Fahrenheit 451 its title?",
-    choiceA : "The main character's nickname",
-    choiceB : "the oversight agency of the government",
-    choiceC : "Taken from the name of the fire department",
-    correct : "It's the temperature at which books burn",
-},
-questionNine : {
-   question : "What type of animal is Boxer in Animal Farm??",
-    choiceA : "Sheep",
-    choiceB : "Dog",
-   choiceC : "Pig",
-    correct : "Horse",
-},
-questionTen : {
-    question : "Complete the quotation: “It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a ___.”?",
-    choiceA : "Title",
-    choiceB : "Larger fortune",
-    choiceC : "House",
-    correct : "Wife",
-}};
 
-function reset(){
-
-stopwatch.time = 120;
-}
-
-
-let aArray = [questions.questionOne.choiceA, questions.questionTwo.choiceA, questions.questionThree.choiceA, questions.questionFour.choiceA, questions.questionFive.choiceA, questions.questionSix.choiceA, questions.questionSeven.choiceA, questions.questionEight.choiceA, questions.questionNine.choiceA, questions.questionTen.choiceA]
-let questionsArray = [questions.questionOne.question, questions.questionTwo.question, questions.questionThree.question, questions.questionFour.question, questions.questionFive.question, questions.questionSix.question, questions.questionSeven.question, questions.questionEight.question, questions.questionNine.question, questions.questionTen.question];
-console.log(aArray)
-    $(".form-check-label").append(questions.questionOne.choiceA)
-    $(".form-check-label2").append(questions.questionOne.choiceB)
-    $(".form-check-label3").append(questions.questionOne.choiceC)
-    $(".form-check-label4").append(questions.questionOne.correct)
-    
-
-questionsArray.forEach(function(i) {  
-        let a = (`<legend>${i}</legend>`); 
-        $(a).addClass("row-form-legend");
-        $(a).attr("data-name", );
-        $(".list").append(a)  
-});
-
-aArray.forEach(function(i){
-$("legend").append(`<label class="form-check-label2">
-<input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">${i}`);
-
-})
-
-//timer
+const quizContainer = document.getElementById('list');
 let timer;
 let  time = 120;
 
+function reset(){
+    stopwatch.time = 120;
+    }
+
+         
 function clock(){
-clearInterval(timer);
-timer = setInterval(count, 1000);
-}
+    clearInterval(timer);
+    timer = setInterval(count, 1000);
+    }
 function count() {
-time--;  
-$(".time").html(`Time Remaining:${time}`);
-if(time === 0){
+    time--;  
+    $(".time").html(`Time Remaining:${time}`);
+    if(time === 0){
     clearInterval(timer);
-}
-}
-if(time === 0){
+     }};
+ if(time === 0){
     clearInterval(timer);
+         };
+
+         clock()
+
+
+// let aArray = [questions.questionOne.choiceA, questions.questionTwo.choiceA, questions.questionThree.choiceA, questions.questionFour.choiceA, questions.questionFive.choiceA, questions.questionSix.choiceA, questions.questionSeven.choiceA, questions.questionEight.choiceA, questions.questionNine.choiceA, questions.questionTen.choiceA]
+// let questionsArray = [questions.questionOne.question, questions.questionTwo.question, questions.questionThree.question, questions.questionFour.question, questions.questionFive.question, questions.questionSix.question, questions.questionSeven.question, questions.questionEight.question, questions.questionNine.question, questions.questionTen.question];
+// console.log(aArray)
+//     $(".form-check-label").append(questions.questionOne.choiceA)
+//     $(".form-check-label2").append(questions.questionOne.choiceB)
+//     $(".form-check-label3").append(questions.questionOne.choiceC)
+//     $(".form-check-label4").append(questions.questionOne.correct)
+
+function startQuiz(){
+    
+    const output = [];
+
+questions.forEach((ask, number) => {  
+    
+    const answers = [];
+
+  let  letter;
+    for (letter in ask.answers) { 
+        answers.push( `<label> <input type="radio" name="question${number}" value="${letter}">
+            ${letter} :
+            ${ask.answers[letter]}
+          </label>`
+        );
+    }
+
+    output.push(
+        `<div class="question"> ${ask.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+        // let a = (`<legend>${ask}</legend>`); 
+        // $(a).addClass("row-form-legend");
+        // // $(a).attr("data-name", );
+        // $(".list").append(a)  
+
+quizContainer.innerHTML = output.join("");
+};
+// aArray.forEach(function(i){
+// $("legend").append(`<label class="form-check-label2">
+// <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">${i}`);
+
+// })
+
+const questions = [{
+    question : "What do the “eyes of Dr. T.J. Eckleburg “represent in The Great Gatsby?",
+    answers : {
+    a :"The Male Gaze",
+    b : "The Green Light",
+    c : "The Eyes of God",
+    d : "Gatsby's subconscious"
+},
+correct : "c"
+},
+{
+question : "what does miss Havisham wear in Great Expectations?",
+answers: { 
+a : "her old school uniform",
+b : "potato sack",
+c : "sailor outfit",
+d : "a wedding dress"
+},
+correct : "d",
+},
+{
+question : "Who wrote all quiet on the Western Front?",
+answers : {
+a : "Erich Maria Remarque",
+b : "Erich Von Schlep",
+c : "Ernest Hemmingway",
+d : "Norman Mailor"
+},
+correct: "a",
+},
+{
+question : "Who does Romeo love in the first scene?",
+answers: {
+a : "Juliet",
+b : "Lady Montague",
+c : "Lady Capulet",
+d : "Rosaline"
+},
+correct: "d"
+},
+{
+question : "Which of the following items does Scout NOT find in a knothole of a tree?",
+answers:{
+a : "gum",
+b : "a small toy car",
+c : "a ball of twine",
+d : "a spelling medal"
+},
+correct: "b",
+},
+{
+question : "What excites Lennie most about his dream life with George?",
+answers : {
+a : "He wants to get back to his roots",
+b : "George promises to introduce him to some hot chicks",
+c : "He will be rich from panninf for gold",
+d : "He will get to tend rabbits"
+},
+correct: "d",
+},
+{
+question : "What is a “big cat“ in the novel Grapes of Wrath",
+answers : {
+a : "A terrible dust storm",
+b : "A machine used by the banks to evict farmers",
+c : "the head farmer",
+d : "A migrants' nickname for a policeman"
+},
+correct: "b"
+},
+{
+question : "What earns Fahrenheit 451 its title?",
+answers: {
+a : "It's the temperature at which books burn",
+b : "the oversight agency of the government",
+c : "Taken from the name of the fire department",
+d : "The main character's nickname"
+},
+correct: "a"
+},
+{
+question : "What type of animal is Boxer in Animal Farm??",
+answers: {
+a : "Sheep",
+b : "Dog",
+c : "Horse",
+d : "Pig"
+},
+correct:  "c"
+},
+{
+question : "Complete the quotation: “It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a ___.”?",
+answers : {
+a : "Title",
+b : "Larger fortune",
+c : "House",
+d : "Wife"
+},
+correct: "d",
 }
-clock()
+];
+startQuiz();
 });
 
 
--- assets/javascript/practice.js
+++ assets/javascript/practice.js
@@ -1,71 +1,243 @@
-const questions = [{
-   {
-            question : "What do the “eyes of Dr. T.J. Eckleburg “represent in The Great Gatsby?",
-            choiceA : "The Male Gaze",
-            choiceB : "The Green Light",
-            choiceC : "Gatsby's subconscious",
-            correct : "The Eyes of God",
-        },
-     {
-        question : "what does miss Havisham wear in Great Expectations?",
-       Answers choiceA : "her old school uniform",
-        choiceB : "potato sack",
-        choiceC : "sailor outfit",
-        correct : "a wedding dress",
-     },
-   {
-        question : "Who wrote all quiet on the Western Front?",
-        choiceA : "Norman Mailor",
-        choiceB : "Erich Von Schlep",
-        choiceC : "Ernest Hemmingway",
-        correct : "Erich Maria Remarque",
+// const questions = [{
+   
+//             question : "What do the “eyes of Dr. T.J. Eckleburg “represent in The Great Gatsby?",
+//             Answers : {
+//             A:"The Male Gaze",
+//             B : "The Green Light",
+//             C : "The Eyes of God",
+//             D : "Gatsby's subconscious",
+//         },
+//         correct : "C"
+//     },
+//      {
+//         question : "what does miss Havisham wear in Great Expectations?",
+//        Answers: { 
+//         A : "her old school uniform",
+//         B : "potato sack",
+//         C : "sailor outfit",
+//         D : "a wedding dress",
+//      },
+//         correct : "D",
+//      },
+//    {
+//       question : "Who wrote all quiet on the Western Front?",
+//         Answers : {
+//         A : "Erich Maria Remarque",
+//         B : "Erich Von Schlep",
+//         C : "Ernest Hemmingway",
+//         D : "Norman Mailor",
+//         },
+//         correct: "A",
+//     },
+//     {
+//         question : "Who does Romeo love in the first scene?",
+//         Answers: {
+//         A : "Juliet",
+//         B : "Lady Montague",
+//         C : "Lady Capulet",
+//         D : "Rosaline",
+//     },
+//     correct: "D"
+// },
+//     {
+//         question : "Which of the following items does Scout NOT find in a knothole of a tree?",
+//         Answers:{
+//         A : "gum",
+//         B : "a small toy car",
+//         C : "a ball of twine",
+//         D : "a spelling medal",
+//     },
+//     correct: "B",
+//     },
+//     {
+//         question : "What excites Lennie most about his dream life with George?",
+//         Answers : {
+//         A : "He wants to get back to his roots",
+//         B : "George promises to introduce him to some hot chicks",
+//         C : "He will be rich from panninf for gold",
+//         D : "He will get to tend rabbits",
+//     },
+//     correct: "D",
+//     },
+//     {
+//         question : "What is a “big cat“ in the novel Grapes of Wrath",
+//         Answers : {
+//         choiceA : "A terrible dust storm",
+//         choiceB : "A machine used by the banks to evict farmers",
+//         choiceC : "the head farmer",
+//         correct : "A migrants' nickname for a policeman",
+//     },
+//     correct: "B"
+//     },
+//    {
+//        question : "What earns Fahrenheit 451 its title?",
+//        Answers: {
+//         choiceA : "It's the temperature at which books burn",
+//         choiceB : "the oversight agency of the government",
+//         choiceC : "Taken from the name of the fire department",
+//         correct : "The main character's nickname",
+//     },
+//     correct: "A"
+//     },
+//    {
+//        question : "What type of animal is Boxer in Animal Farm??",
+//        Answers: {
+//         A : "Sheep",
+//         B : "Dog",
+//         C : "Horse",
+//         D : "Pig",
+//     },
+//     correct:  "C"
+//     },
+//      {
+//         question : "Complete the quotation: “It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a ___.”?",
+//         Answers :{
+//         A : "Title",
+//         B : "Larger fortune",
+//         C : "House",
+//         D : "Wife",
+//         },
+//         correct: "D",
+
+//     }];
+
+//     <div class="container">
+     
+//     <div class="row"> 
+//         <div class= 'col-xs-2'></div>
+//         <h1>Did you actually read that?</h1>  
+//         <p>Sparknotes can't help you now. You have 120 seconds to recall facts from all the books you should have read during high-school. Good luck. </p>
+//     </div>  
+// </div>
+// <div class= 'col-xs-2'></div>
+// <div class = "row">
+//     <p class="time"></p:>
+// </div>
+//     <div class ="row">
+//     <div class= "col-xs-2"></div>
+//    <legend class="row-form-legend">What do the "eyes of Dr. T.J. Eckleburg" represent in The Great Gatsby?</legend> 
+//     </div>
+//    <div class ="form-group row">
+//     <div class= "col-xs-2"></div>
+//     <div class="col-sm-10">
+//         <label class="form-check-label">
+//         <input class="form-check-input" type="radio" name="gridRadios" id="exampleRadios1" value="option1"checked>
+//         </label>
+//             <label class="form-check-label2">
+//             <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+//             </label>
+//                 <label class="form-check-label3">
+//                 <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3">
+//                 </label>
+//             <label class="form-check-label4">
+//             <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios4" value="option4">
+//           </label>
+
+
+//           HTML  CSS  JS  Result
+// EDIT ON
+ (function() {
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      const answers = [];
+
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    {
+      question: "Who is the strongest?",
+      answers: {
+        a: "Superman",
+        b: "The Terminator",
+        c: "Waluigi, obviously"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is the best site ever created?",
+      answers: {
+        a: "SitePoint",
+        b: "Simple Steps Code",
+        c: "Trick question; they're both the best"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Where is Waldo really?",
+      answers: {
+        a: "Antarctica",
+        b: "Exploring the Pacific Ocean",
+        c: "Sitting in a tree",
+        d: "Minding his own business, so stop asking"
+      },
+      correctAnswer: "d"
     }
-    {
-        question : "Who does Romeo love in the first scene?",
-        choiceA : "Juliet",
-        choiceB : "Lady Montague",
-        choiceC : "Lady Capulet",
-        correct : "Rosaline",
-    },
-    {
-        question : "Which of the following items does Scout NOT find in a knothole of a tree?",
-        choiceA : "gum",
-        choiceB : "a spelling medal",
-        choiceC : "a ball of twine",
-        correct : "a small toy car",
-    },
-    {
-        question : "What excites Lennie most about his dream life with George?",
-        choiceA : "He wants to get back to his roots",
-        choiceB : "George promises to introduce him to some hot chicks",
-        choiceC : "He will be rich from panninf for gold",
-        correct : "He will get to tend rabbits",
-    },
-    {
-        question : "What is a “big cat“ in the novel Grapes of Wrath",
-        choiceA : "A terrible dust storm",
-        choiceB : "A migrants' nickname for a policeman",
-        choiceC : "the head farmer",
-        correct : "A machine used by the banks to evict farmers",
-    },
-   {
-       question : "What earns Fahrenheit 451 its title?",
-        choiceA : "The main character's nickname",
-        choiceB : "the oversight agency of the government",
-        choiceC : "Taken from the name of the fire department",
-        correct : "It's the temperature at which books burn",
-    },
-   {
-       question : "What type of animal is Boxer in Animal Farm??",
-        choiceA : "Sheep",
-        choiceB : "Dog",
-       choiceC : "Pig",
-        correct : "Horse",
-    },
-     {
-        question : "Complete the quotation: “It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a ___.”?",
-        choiceA : "Title",
-        choiceB : "Larger fortune",
-        choiceC : "House",
-        correct : "Wife",
-    }];
+  ];
+
+  // display quiz right away
+  buildQuiz();
+
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
+})();
